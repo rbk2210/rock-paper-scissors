17337c43a6626c0c65884768d9e4dd1babf62911
Diego <diegoysole2210@gmail.com>
2024-11-29 19:10:12 -0300

    solution Image

--- js/script.js
+++ js/script.js
@@ -29,220 +29,221 @@ const winlose = document.querySelector('.win-lose')
 
 
 
-
+cpuBox.style.animation = 'changeImage 1s infinite'
 
 function playGame() {
-    
     message.style.animation = "none";
     winlose.style.color = 'black'
     let randomNum = Math.floor(Math.random() * 3) + 1
     if (playerSelected === '') {
         message.style.color = 'red';
         message.textContent = 'Por favor elije una opcion'
-        cpuBox.style.animation = 'animationvs 1s infinite'
+        cpuBox.style.animation = 'changeImage 1s infinite'
+    } else {
+        if (randomNum === 1) {
+            cpuSlected = 'rock'
+            cpuBox.style.animation = 'none'
+            cpuBox.style.transform = "translateY(0)"
+            cpuBox.style.backgroundImage = "url(./img/rock.png)";
+        } else if (randomNum === 2) {
+            cpuSlected = 'paper'
+            cpuBox.style.animation = 'none'
+            cpuBox.style.transform = "translateY(0)"
+            cpuBox.style.backgroundImage = "url(./img/paper.png)";
+        } else if (randomNum === 3) {
+            cpuSlected = 'scissors'
+            cpuBox.style.animation = 'none'
+            cpuBox.style.transform = "translateY(0)"
+            cpuBox.style.backgroundImage = "url(./img/scissors.png)";
+        }
+        /* Selected rock */
+        if (playerSelected === 'rock' && cpuSlected === 'scissors') {
+            message.textContent = 'Piedra gana a tijeras'
+            playerBox.style.animation = 'none'
+            playerBox.style.transform = "translateY(0)"
+            playerBox.style.backgroundImage = "url(./img/rock.png)";
+            message.style.color = 'green'
+            lifeCpu -= 1
+            if (lifeCpu === 2) {
+                cpuh3.style.animation = 'none'
+                cpuh3.style.filter = 'brightness(0) invert(0)'
+            } else if (lifeCpu === 1) {
+                cpuh2.style.animation = 'none'
+                cpuh2.style.filter = 'brightness(0) invert(0)'
+            } else if (lifeCpu === 0) {
+                cpuh1.style.animation = 'none'
+                cpuh1.style.filter = 'brightness(0) invert(0)'
+                continuebtn.disabled = false
+                playbtn.disabled = true
+                rock.disabled = true
+                paper.disabled = true
+                scissors.disabled = true
+                setTimeout(() => {
+                    winlose.textContent = '¡Ganaste!'
+                    winlose.style.color = 'green'
+                }, 1)
+            }
+        } else if (playerSelected === 'rock' && cpuSlected === 'rock') {
+            message.textContent = 'Empate'
+            playerBox.style.animation = 'none'
+            playerBox.style.transform = "translateY(0)"
+            playerBox.style.backgroundImage = "url(./img/rock.png)"
+            message.style.color = 'black'
+            message.style.animation = "draw .5s ease-in-out";
+        } else if (playerSelected === 'rock' && cpuSlected === 'paper') {
+            message.textContent = 'Papel gana a piedra'
+            playerBox.style.animation = 'none'
+            playerBox.style.transform = "translateY(0)"
+            playerBox.style.backgroundImage = "url(./img/rock.png)"
+            message.style.color = 'red'
+            lifePlayer -= 1
+            if (lifePlayer === 2) {
+                ph3.style.animation = 'none'
+                ph3.style.filter = 'brightness(0) invert(0)'
+            } else if (lifePlayer === 1) {
+                ph2.style.animation = 'none'
+                ph2.style.filter = 'brightness(0) invert(0)'
+            } else if (lifePlayer === 0) {
+                ph1.style.animation = 'none'
+                ph1.style.filter = 'brightness(0) invert(0)'
+                continuebtn.disabled = false
+                playbtn.disabled = true
+                rock.disabled = true
+                paper.disabled = true
+                scissors.disabled = true
+                setTimeout(() => {
+                    winlose.textContent = '¡Perdiste!'
+                    winlose.style.color = 'red'
+                }, 1)
+            }
+        }
+
+        /* Selected paper */
+        if (playerSelected === 'paper' && cpuSlected === 'scissors') {
+            message.style.color = 'red'
+            playerBox.style.animation = 'none'
+            playerBox.style.transform = "translateY(0)"
+            playerBox.style.backgroundImage = "url(./img/paper.png)";
+            message.textContent = 'Tijeras ganan a papel'
+            lifePlayer -= 1
+            if (lifePlayer === 2) {
+                ph3.style.animation = 'none'
+                ph3.style.filter = 'brightness(0) invert(0)'
+            } else if (lifePlayer === 1) {
+                ph2.style.animation = 'none'
+                ph2.style.filter = 'brightness(0) invert(0)'
+            } else if (lifePlayer === 0) {
+                ph1.style.animation = 'none'
+                ph1.style.filter = 'brightness(0) invert(0)'
+                continuebtn.disabled = false
+                playbtn.disabled = true
+                rock.disabled = true
+                paper.disabled = true
+                scissors.disabled = true
+                setTimeout(() => {
+                    winlose.textContent = '¡Perdiste!'
+                    winlose.style.color = 'red'
+                }, 1)
+            }
+        } else if (playerSelected === 'paper' && cpuSlected === 'rock') {
+            message.style.color = 'green'
+            playerBox.style.animation = 'none'
+            playerBox.style.transform = "translateY(0)"
+            playerBox.style.backgroundImage = "url(./img/paper.png)";
+            message.textContent = 'Papel gana a piedra'
+            lifeCpu -= 1
+            if (lifeCpu === 2) {
+                cpuh3.style.animation = 'none'
+                cpuh3.style.filter = 'brightness(0) invert(0)'
+            } else if (lifeCpu === 1) {
+                cpuh2.style.animation = 'none'
+                cpuh2.style.filter = 'brightness(0) invert(0)'
+            } else if (lifeCpu === 0) {
+                cpuh1.style.animation = 'none'
+                cpuh1.style.filter = 'brightness(0) invert(0)'
+                continuebtn.disabled = false
+                playbtn.disabled = true
+                rock.disabled = true
+                paper.disabled = true
+                scissors.disabled = true
+                setTimeout(() => {
+                    winlose.textContent = '¡Ganaste!'
+                    winlose.style.color = 'green'
+                }, 1)
+            }
+        } else if (playerSelected === 'paper' && cpuSlected === 'paper') {
+            message.style.color = 'black'
+            playerBox.style.animation = 'none'
+            playerBox.style.transform = "translateY(0)"
+            playerBox.style.backgroundImage = "url(./img/paper.png)";
+            message.textContent = 'Empate'
+            message.style.animation = "draw .5s ease-in-out";
+        }
+
+        /* Selected scissors */
+        if (playerSelected === 'scissors' && cpuSlected === 'scissors') {
+            message.style.color = 'black'
+            playerBox.style.animation = 'none'
+            playerBox.style.transform = "translateY(0)"
+            playerBox.style.backgroundImage = "url(./img/scissors.png)";
+            message.textContent = 'Empate'
+            message.style.animation = "draw .5s ease-in-out";
+        } else if (playerSelected === 'scissors' && cpuSlected === 'rock') {
+            message.style.color = 'red'
+            playerBox.style.animation = 'none'
+            playerBox.style.transform = "translateY(0)"
+            playerBox.style.backgroundImage = "url(./img/scissors.png)";
+            message.textContent = 'Piedra gana a tijeras'
+            lifePlayer -= 1
+            if (lifePlayer === 2) {
+                ph3.style.animation = 'none'
+                ph3.style.filter = 'brightness(0) invert(0)'
+            } else if (lifePlayer === 1) {
+                ph2.style.animation = 'none'
+                ph2.style.filter = 'brightness(0) invert(0)'
+            } else if (lifePlayer === 0) {
+                ph1.style.animation = 'none'
+                ph1.style.filter = 'brightness(0) invert(0)'
+                continuebtn.disabled = false
+                playbtn.disabled = true
+                rock.disabled = true
+                paper.disabled = true
+                scissors.disabled = true
+                setTimeout(() => {
+                    winlose.textContent = '¡Perdiste!'
+                    winlose.style.color = 'red'
+                }, 1)
+            }
+        } else if (playerSelected === 'scissors' && cpuSlected === 'paper') {
+            message.textContent = 'Tijeras gana a papel'
+            playerBox.style.animation = 'none'
+            playerBox.style.transform = "translateY(0)"
+            playerBox.style.backgroundImage = "url(./img/scissors.png)";
+            message.style.color = 'green'
+            lifeCpu -= 1
+            if (lifeCpu === 2) {
+                cpuh3.style.animation = 'none'
+                cpuh3.style.filter = 'brightness(0) invert(0)'
+            } else if (lifeCpu === 1) {
+                cpuh2.style.animation = 'none'
+                cpuh2.style.filter = 'brightness(0) invert(0)'
+            } else if (lifeCpu === 0) {
+                cpuh1.style.animation = 'none'
+                cpuh1.style.filter = 'brightness(0) invert(0)'
+                continuebtn.disabled = false
+                playbtn.disabled = true
+                rock.disabled = true
+                paper.disabled = true
+                scissors.disabled = true
+                setTimeout(() => {
+                    winlose.textContent = '¡Ganaste!'
+                    winlose.style.color = 'green'
+                }, 1)
+            }
+        }
     }
 
-    if (randomNum === 1) {
-        cpuSlected = 'rock'
-        cpuBox.style.animation = 'none'
-        cpuBox.style.transform = "translateY(0)"
-        cpuBox.style.backgroundImage = "url(../img/rock.png)";
-    } else if (randomNum === 2) {
-        cpuSlected = 'paper'
-        cpuBox.style.animation = 'none'
-        cpuBox.style.transform = "translateY(0)"
-        cpuBox.style.backgroundImage = "url(../img/paper.png)";
-    } else if (randomNum === 3) {
-        cpuSlected = 'scissors'
-        cpuBox.style.animation = 'none'
-        cpuBox.style.transform = "translateY(0)"
-        cpuBox.style.backgroundImage = "url(../img/scissors.png)";
-    }
-    /* Selected rock */
-    if (playerSelected === 'rock' && cpuSlected === 'scissors') {
-        message.textContent = 'Piedra gana a tijeras'
-        playerBox.style.animation = 'none'
-        playerBox.style.transform = "translateY(0)"
-        playerBox.style.backgroundImage = "url(../img/rock.png)";
-        message.style.color = 'green'
-        lifeCpu -= 1
-        if (lifeCpu === 2) {
-            cpuh3.style.animation = 'none'
-            cpuh3.style.filter = 'brightness(0) invert(0)'
-        } else if (lifeCpu === 1) {
-            cpuh2.style.animation = 'none'
-            cpuh2.style.filter = 'brightness(0) invert(0)'
-        } else if (lifeCpu === 0) {
-            cpuh1.style.animation = 'none'
-            cpuh1.style.filter = 'brightness(0) invert(0)'
-            continuebtn.disabled = false
-            playbtn.disabled = true
-            rock.disabled = true
-            paper.disabled = true
-            scissors.disabled = true
-            setTimeout(() => {
-                winlose.textContent = '¡Ganaste!'
-                winlose.style.color = 'green'
-            }, 1)
-        }
-    } else if (playerSelected === 'rock' && cpuSlected === 'rock') {
-        message.textContent = 'Empate'
-        playerBox.style.animation = 'none'
-        playerBox.style.transform = "translateY(0)"
-        playerBox.style.backgroundImage = "url(../img/rock.png)"
-        message.style.color = 'black'
-        message.style.animation = "draw .5s ease-in-out";
-    } else if (playerSelected === 'rock' && cpuSlected === 'paper') {
-        message.textContent = 'Papel gana a piedra'
-        playerBox.style.animation = 'none'
-        playerBox.style.transform = "translateY(0)"
-        playerBox.style.backgroundImage = "url(../img/rock.png)"
-        message.style.color = 'red'
-        lifePlayer -= 1
-        if (lifePlayer === 2) {
-            ph3.style.animation = 'none'
-            ph3.style.filter = 'brightness(0) invert(0)'
-        } else if (lifePlayer === 1) {
-            ph2.style.animation = 'none'
-            ph2.style.filter = 'brightness(0) invert(0)'
-        } else if (lifePlayer === 0) {
-            ph1.style.animation = 'none'
-            ph1.style.filter = 'brightness(0) invert(0)'
-            continuebtn.disabled = false
-            playbtn.disabled = true
-            rock.disabled = true
-            paper.disabled = true
-            scissors.disabled = true
-            setTimeout(() => {
-                winlose.textContent = '¡Perdiste!'
-                winlose.style.color = 'red'
-            }, 1)
-        }
-    }
 
-    /* Selected paper */
-    if (playerSelected === 'paper' && cpuSlected === 'scissors') {
-        message.style.color = 'red'
-        playerBox.style.animation = 'none'
-        playerBox.style.transform = "translateY(0)"
-        playerBox.style.backgroundImage = "url(../img/paper.png)";
-        message.textContent = 'Tijeras ganan a papel'
-        lifePlayer -= 1
-        if (lifePlayer === 2) {
-            ph3.style.animation = 'none'
-            ph3.style.filter = 'brightness(0) invert(0)'
-        } else if (lifePlayer === 1) {
-            ph2.style.animation = 'none'
-            ph2.style.filter = 'brightness(0) invert(0)'
-        } else if (lifePlayer === 0) {
-            ph1.style.animation = 'none'
-            ph1.style.filter = 'brightness(0) invert(0)'
-            continuebtn.disabled = false
-            playbtn.disabled = true
-            rock.disabled = true
-            paper.disabled = true
-            scissors.disabled = true
-            setTimeout(() => {
-                winlose.textContent = '¡Perdiste!'
-                winlose.style.color = 'red'
-            }, 1)
-        }
-    } else if (playerSelected === 'paper' && cpuSlected === 'rock') {
-        message.style.color = 'green'
-        playerBox.style.animation = 'none'
-        playerBox.style.transform = "translateY(0)"
-        playerBox.style.backgroundImage = "url(../img/paper.png)";
-        message.textContent = 'Papel gana a piedra'
-        lifeCpu -= 1
-        if (lifeCpu === 2) {
-            cpuh3.style.animation = 'none'
-            cpuh3.style.filter = 'brightness(0) invert(0)'
-        } else if (lifeCpu === 1) {
-            cpuh2.style.animation = 'none'
-            cpuh2.style.filter = 'brightness(0) invert(0)'
-        } else if (lifeCpu === 0) {
-            cpuh1.style.animation = 'none'
-            cpuh1.style.filter = 'brightness(0) invert(0)'
-            continuebtn.disabled = false
-            playbtn.disabled = true
-            rock.disabled = true
-            paper.disabled = true
-            scissors.disabled = true
-            setTimeout(() => {
-                winlose.textContent = '¡Ganaste!'
-                winlose.style.color = 'green'
-            }, 1)
-        }
-    } else if (playerSelected === 'paper' && cpuSlected === 'paper') {
-        message.style.color = 'black'
-        playerBox.style.animation = 'none'
-        playerBox.style.transform = "translateY(0)"
-        playerBox.style.backgroundImage = "url(../img/paper.png)";
-        message.textContent = 'Empate'
-        message.style.animation = "draw .5s ease-in-out";
-    }
-
-    /* Selected scissors */
-    if (playerSelected === 'scissors' && cpuSlected === 'scissors') {
-        message.style.color = 'black'
-        playerBox.style.animation = 'none'
-        playerBox.style.transform = "translateY(0)"
-        playerBox.style.backgroundImage = "url(../img/scissors.png)";
-        message.textContent = 'Empate'
-        message.style.animation = "draw .5s ease-in-out";
-    } else if (playerSelected === 'scissors' && cpuSlected === 'rock') {
-        message.style.color = 'red'
-        playerBox.style.animation = 'none'
-        playerBox.style.transform = "translateY(0)"
-        playerBox.style.backgroundImage = "url(../img/scissors.png)";
-        message.textContent = 'Piedra gana a tijeras'
-        lifePlayer -= 1
-        if (lifePlayer === 2) {
-            ph3.style.animation = 'none'
-            ph3.style.filter = 'brightness(0) invert(0)'
-        } else if (lifePlayer === 1) {
-            ph2.style.animation = 'none'
-            ph2.style.filter = 'brightness(0) invert(0)'
-        } else if (lifePlayer === 0) {
-            ph1.style.animation = 'none'
-            ph1.style.filter = 'brightness(0) invert(0)'
-            continuebtn.disabled = false
-            playbtn.disabled = true
-            rock.disabled = true
-            paper.disabled = true
-            scissors.disabled = true
-            setTimeout(() => {
-                winlose.textContent = '¡Perdiste!'
-                winlose.style.color = 'red'
-            }, 1)
-        }
-    } else if (playerSelected === 'scissors' && cpuSlected === 'paper') {
-        message.textContent = 'Tijeras gana a papel'
-        playerBox.style.animation = 'none'
-        playerBox.style.transform = "translateY(0)"
-        playerBox.style.backgroundImage = "url(../img/scissors.png)";
-        message.style.color = 'green'
-        lifeCpu -= 1
-        if (lifeCpu === 2) {
-            cpuh3.style.animation = 'none'
-            cpuh3.style.filter = 'brightness(0) invert(0)'
-        } else if (lifeCpu === 1) {
-            cpuh2.style.animation = 'none'
-            cpuh2.style.filter = 'brightness(0) invert(0)'
-        } else if (lifeCpu === 0) {
-            cpuh1.style.animation = 'none'
-            cpuh1.style.filter = 'brightness(0) invert(0)'
-            continuebtn.disabled = false
-            playbtn.disabled = true
-            rock.disabled = true
-            paper.disabled = true
-            scissors.disabled = true
-            setTimeout(() => {
-                winlose.textContent = '¡Ganaste!'
-                winlose.style.color = 'green'
-            }, 1)
-        }
-    }
 }
 
 rockButton.addEventListener('click', () => {
